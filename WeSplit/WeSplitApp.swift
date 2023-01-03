005ce4836cf83eccf0636626e7a92c323455de54
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-03 10:37:41 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Maurício Costa on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
